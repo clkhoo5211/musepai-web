40b9a8b1cb999a11cc2bfaf11cd6a803dcf90d2a
clkhoo5211 <clkhoo@iotatechs.com>
2026-09-23 18:20:53 +0800

    MusePAI web build 2026-09-23

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1494904233" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3150668415" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
